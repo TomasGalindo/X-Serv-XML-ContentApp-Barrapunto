4744dd936b08be9ab8f537af318ec639552d7938
TomasGalindo <tgalindoencinas@yahoo.com>
2017-04-16 17:21:32 +0200

    Utilizando WebApp

---
+++ Version_1/contentapp.py
@@ -0,0 +1,52 @@
+#! /usr/bin/python3
+import xml_parser_barrapunto
+from xml.sax import make_parser
+import urllib.request
+#import sys
+import webapp
+
+class contentApp (webapp.webApp):
+    content = {'/': 'Root page',
+                '/page': 'A page'
+                }
+    def parse(self,request):
+        metodo = request.split(' ',1)[0]
+        recurso = request.split(' ',2)[1]
+        body = request.split('\r\n\r\n')[1]
+
+        return (metodo,recurso,body)
+
+    def process(self,parsed):
+        metodo,resourceName,body = parsed
+        if metodo == "GET":
+            if resourceName in self.content.keys():
+                # MIra en el dicionario y saca el recurso
+                #hacer todo lo del parser
+                theParser = make_parser()
+                theHandler = xml_parser_barrapunto.myContentHandler()
+                theParser.setContentHandler(theHandler)
+                url = "http://barrapunto.com/index.rss"
+                f = urllib.request.urlopen(url)
+                theParser.parse(f)
+                #saco el contetn_Rss
+                content_Rss = theHandler.texto
+                httpCode = "200 OK"
+                htmlBody = ("<html><body><div>Pagina buscada: " + self.content[resourceName] +
+                            "</div><div>Noticias Barrapunto: " + content_Rss +
+                            "</div></body></html>")
+            else:
+                httpCode = "404 Not Found"
+                htmlBody = "Element Not Found"
+        elif metodo == "PUT":
+            #Tengo que guardar las cosas en el diccionario
+            self.content[resourceName] = body
+            httpCode = "200 OK"
+            htmlBody = "PUT Detectado. Guardado en  el diccionario"
+        else:
+            httpCode = "405 Method Not Allowed"
+            htmlBody = "No se puede utilizar ese método"
+
+        return (httpCode, htmlBody)
+
+if __name__ == "__main__":
+    testWebApp = contentApp("localhost", 1234)
---
+++ Version_1/xml_parser_barrapunto.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#
+# Simple XML parser for the RSS channel from BarraPunto
+# Jesus M. Gonzalez-Barahona
+# jgb @ gsyc.es
+# TSAI and SAT subjects (Universidad Rey Juan Carlos)
+# September 2009
+#
+# Just prints the news (and urls) in BarraPunto.com,
+#  after reading the corresponding RSS channel.
+
+from xml.sax.handler import ContentHandler
+from xml.sax import make_parser
+import sys
+import urllib.request
+
+
+# Para ejecutarlo:
+# python2.7 xml-parser-barrapunto.py >barrapunto.html
+# lo ultimo es para que la salida estandar la guarde
+# en un fichero
+
+class myContentHandler(ContentHandler):
+    texto= ""
+    def __init__(self):
+        self.texto += "<html><body>"
+        self.inItem = False
+        self.inContent = False
+        self.theContent = ""
+
+    def startElement(self, name, attrs):
+        if name == 'item':
+            self.inItem = True
+        elif self.inItem:
+            if name == 'title':
+                self.inContent = True
+            elif name == 'link':
+                self.inContent = True
+
+    def endElement(self, name):
+        if name == 'item':
+            self.inItem = False
+        elif self.inItem:
+            if name == 'title':
+                self.texto += ("<div><p>Title: " + self.theContent + ".</p>")
+                # To avoid Unicode trouble
+                self.inContent = False
+                self.theContent = ""
+            elif name == 'link':
+                self.texto += ("<p>Link: " + "<a href=" + self.theContent + ">" + self.theContent + "</a></p></div>")
+                self.inContent = False
+                self.theContent = ""
+
+    def characters(self, chars):
+        if self.inContent:
+            self.theContent = self.theContent + chars
