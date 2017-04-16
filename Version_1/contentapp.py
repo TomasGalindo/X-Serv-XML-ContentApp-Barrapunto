8e2587d7e704a1418f82ead95882997146895737
TomasGalindo <tgalindoencinas@yahoo.com>
2017-04-16 17:27:07 +0200

    Update contentapp.py

--- Version_1/contentapp.py
+++ Version_1/contentapp.py
@@ -2,43 +2,45 @@
 import xml_parser_barrapunto
 from xml.sax import make_parser
 import urllib.request
-#import sys
 import webapp
+
 
 class contentApp (webapp.webApp):
     content = {'/': 'Root page',
-                '/page': 'A page'
-                }
-    def parse(self,request):
-        metodo = request.split(' ',1)[0]
-        recurso = request.split(' ',2)[1]
+               '/page': 'A page'
+              }
+    
+    def parse(self, request):
+        metodo = request.split(' ', 1)[0]
+        recurso = request.split(' ', 2)[1]
         body = request.split('\r\n\r\n')[1]
 
-        return (metodo,recurso,body)
+        return (metodo, recurso, body)
 
     def process(self,parsed):
-        metodo,resourceName,body = parsed
+        metodo, resourceName, body = parsed
         if metodo == "GET":
             if resourceName in self.content.keys():
                 # MIra en el dicionario y saca el recurso
-                #hacer todo lo del parser
+                # hacer todo lo del parser
                 theParser = make_parser()
                 theHandler = xml_parser_barrapunto.myContentHandler()
                 theParser.setContentHandler(theHandler)
                 url = "http://barrapunto.com/index.rss"
                 f = urllib.request.urlopen(url)
                 theParser.parse(f)
-                #saco el contetn_Rss
+                # saco el contetn_Rss
                 content_Rss = theHandler.texto
                 httpCode = "200 OK"
-                htmlBody = ("<html><body><div>Pagina buscada: " + self.content[resourceName] +
+                htmlBody = ("<html><body><div>Pagina buscada: " +
+                            self.content[resourceName] +
                             "</div><div>Noticias Barrapunto: " + content_Rss +
                             "</div></body></html>")
             else:
                 httpCode = "404 Not Found"
                 htmlBody = "Element Not Found"
         elif metodo == "PUT":
-            #Tengo que guardar las cosas en el diccionario
+            # Tengo que guardar las cosas en el diccionario
             self.content[resourceName] = body
             httpCode = "200 OK"
             htmlBody = "PUT Detectado. Guardado en  el diccionario"
